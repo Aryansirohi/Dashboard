1afd6e02833a2c3d1488d32db8ab94131e4fbf9f
Aryan <sirohiaryan879@gmail.com>
2025-01-05 22:16:07 +0530

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import { Route, Routes } from "react-router-dom"
-import OverviewPage from "./pages/OverviewPage"
-import ProductsPage from "./pages/ProductsPage"
+import { Route, Routes } from "react-router-dom";
+import OverviewPage from "./pages/OverviewPage";
+import ProductsPage from "./pages/ProductsPage";
+import Sidebar from "./components/SideBar";
+import Userpage from "./pages/Userpage";
+import SalesPage from "./pages/SalesPage";
+import OrderPage from "./pages/OrderPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
 
 function App() {
-
-
   return (
     <>
       <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+        {/* {Background} */}
 
-<div className="fixed-inset-0 z-0">  
-    <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80"/>
-</div>
-
+        <div className="fixed-inset-0 z-0">
+          <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
+          <div className="absolute inset-0 backdrop-blur-sm" />
+          
+        </div>
+        <Sidebar />
         <Routes>
-          <Route path="/" element={<OverviewPage/>} />
-          <Route path="/products" element={<ProductsPage/>} />
+
+          <Route path="/overview" element={<OverviewPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/users" element={<Userpage />} />
+          <Route path="/sales" element={<SalesPage />} />
+          <Route path="/orders" element={<OrderPage />} />
+          <Route path="/analytics" element={<AnalyticsPage />} />
+
         </Routes>
-       
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
